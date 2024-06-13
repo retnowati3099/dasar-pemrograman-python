880b8244b1d0a9214dc9d015b889a16e9c32a304
Retno Wati <retnowati3099@gmail.com>
2024-06-13 09:55:00 +0700

    fungsi input

---
+++ fungsi-input.py
@@ -0,0 +1,9 @@
+str_input = input("Enter your grade: ")
+grade = int(str_input)
+print("inputan user", grade, type(grade))
+
+'''
+- Fungsi input() digunakan untuk menampilkan suatu pesan teks (yang disisipkan saat fungsi dipanggil) dan mengembalikan nilai inputan user dalam bentuk string.
+- Fungsi type() digunakan untuk melihat informasi tipe data dari suatu nilai atau variabel. Fungsi ini mengembalikan string dalam format <class 'tipe_data'>.
+- Konversi tipe data string ke int dilakukan menggunakan fungsi int()
+'''
