9d9d688f3ff99f814457d2445b9e3481c2380fc4
Retno Wati <retnowati3099@gmail.com>
2024-06-17 12:41:15 +0700

    ternary dengan nilai balik

--- seleksi-kondisi-sebaris-dan-ternary.py
+++ seleksi-kondisi-sebaris-dan-ternary.py
@@ -15,7 +15,12 @@ if grade < 65:
 # ternary
 print("passed the exam") if grade >= 60 else print("below the passing grade")
 
+# ternary dengan nilai balik
+message = "passed the exam" if grade >= 60 else "below the passing grade"
+print(message)
+
 """
 - Metode penulisan sebaris cocok diterapkan pada situasi dimana seleksi kondisi hanya memiliki 1 kondisi saja.
 - Metode penulisan ternary umum diterapkan pada blok kode seleksi kondisi yang memiliki 2 kondisi (True dan False)
+- Ternary dengan nilai balik sama seperti penerapan ternary sebelumnya, pembedanya pada metode ini setiap kondisi menghasilkannilai balik yang umumnya ditampung oleh variabel.
 """
