8e8287c94bc42d37699a3969d3815d358febe135
Retno Wati <retnowati3099@gmail.com>
2024-07-14 07:46:18 +0700

    nested list dan konversi tipe data lain ke list

--- tipe-data-list.py
+++ tipe-data-list.py
@@ -39,10 +39,47 @@ for i in range(0, len(numbers)):
 for index, value in enumerate(numbers):
     print("elemen", index, ":", value)
 
+# nested list
+matrix = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+for row in matrix:
+    for cel in row:
+        print(cel, end=" ")
+    print()
+
+# konversi range ke list
+# list dimulai angka 0 hingga 9
+range_1 = range(0, 10)
+print(list(range_1))
+
+# list dimulai dari angka 1 hingga 21 dengan penambahan 3
+range_2 = range(1, 22, 3)
+print(list(range_2))
+
+# list dimulai dari angka 100 hingga 0 dengan pengurangan -10
+range_3 = range(100, 0, -10)
+print(list(range_3))
+
+# konversi string ke list
+full_name = "Retno Wati"
+print(list(full_name))
+
+# konversi tuple ke list
+tuple_1 = (1, 2, 3, 4)
+print(list(tuple_1))
+
+# mengecek apakah elemen ada
+
+
 """
-- List adalah tipe data untuk menampung nilai kolektif yang disimpan secara urut, dengan isi bisa berupa banyak varian tipe data (tidak harus sejenis)
+- List adalah tipe data untuk menampung nilai kolektif yang disimpan secara urut, dengan isi bisa berupa banyak varian tipe data (tidak harus sejenis).
 - Penerapan list adalah dengan menuliskan nilai kolektif dengan pembatas tanda koma dan diapit tanda kurung siku.
 - Data dalam list biasa disebut dengan elemen.
-- Setiap elemen disimpan di list secara urut dengan penanda urutan yang disebut indeks
-- Pengaksesan elemen list menggunakan notasi nama_list[indeks]
+- Setiap elemen disimpan di list secara urut dengan penanda urutan yang disebut indeks.
+- Pengaksesan elemen list menggunakan notasi nama_list[indeks].
+- Pengaksesanelemen menggunakan indeks di luar kapasitas data akan menghasilkan error.
 """
